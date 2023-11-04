48fe778aafa033248513e11b4fb27feb5a6b68a4
shunsuke250 <soeyamashunsuke@gmail.com>
2023-11-04 16:58:02 +0900

    :sparkles: プロジェクトを作成

---
+++ SwiftUITest23-11-04/SwiftUITest23-11-04/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUITest23-11-04
+//
+//  Created by 副山俊輔 on 2023/11/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUITest23-11-04/SwiftUITest23-11-04/SwiftUITest23_11_04App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITest23_11_04App.swift
+//  SwiftUITest23-11-04
+//
+//  Created by 副山俊輔 on 2023/11/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITest23_11_04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
